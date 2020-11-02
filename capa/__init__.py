c73bf9cbff204ed6b6a2d5abe231f74ca097aa43
Kyle McCormick <kmccormick@edx.org>
2020-11-02 17:24:37 -0500

    Delete unused code

--- capa/__init__.py
+++ capa/__init__.py
@@ -3,5 +3,3 @@ edX LON-CAPA Problem Support.
 """
 
 __version__ = '0.1.0'
-
-default_app_config = 'capa.apps.CapaConfig'  # pylint: disable=invalid-name
--- capa/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-URLs for capa.
-"""
-from django.conf.urls import url
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="capa/base.html")),
-]
